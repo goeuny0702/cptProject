e18f4d82daa5b810299ad09629cdf3e8966cd40d
goeuny0702 <zz4442@naver.com>
2025-02-02 15:37:21 +0900

    hi

---
+++ src/test/java/com/example/cptProject/CptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cptProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CptProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
